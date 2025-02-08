92f2856be5d7d5c60909d3fe677fd91800d7d99a
Akasukpe Aghoghomena <aghoghomena3@gmail.com>
2025-02-07 22:53:11 -0500

    Added fail safe to docker pruning command

--- scripts/stop_old_container.sh
+++ scripts/stop_old_container.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Stopping running containers..."
 cd /home/ec2-user/app || exit 1
-sudo docker-compose -f docker-compose.prod.yml down
+sudo docker-compose -f docker-compose.prod.yml down || true
 
 echo "Removing unused Docker images..."
-sudo docker image prune -af
+sudo docker image prune -af || true
 
 echo "Existing containers stopped and removed."
