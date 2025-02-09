eee0a5401e2f8833af6a3b2efbab05f8619aead5
Akasukpe Aghoghomena <aghoghomena3@gmail.com>
2025-02-09 16:32:50 -0500

    removed comments

--- scripts/start_new_container.sh
+++ scripts/start_new_container.sh
@@ -26,12 +26,6 @@ sudo docker-compose -f docker-compose.prod.yml up --build -d
 echo "Waiting for database to be ready..."
 sleep 10  # Ensure PostgreSQL starts before running migrations
 
-echo "Running Django migrations..."
-# sudo docker-compose -f docker-compose.prod.yml exec -T backend python manage.py migrate
-
-echo "Collecting static files..."
-# sudo docker-compose -f docker-compose.prod.yml exec -T backend python manage.py collectstatic --noinput
-
 echo "Restarting containers..."
 sudo docker-compose -f docker-compose.prod.yml restart
 
