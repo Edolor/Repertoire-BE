45dbc837bfe4525a6882b27141b9ab1c1466247d
Akasukpe Aghoghomena <aghoghomena3@gmail.com>
2025-02-09 15:00:45 -0500

    Removed intentional error

--- scripts/dump_to_s3.sh
+++ scripts/dump_to_s3.sh
@@ -7,7 +7,7 @@ BACKUP_FILE="$BASE_PATH/web/db_backup.json"
 FILE_NAME="db_backup.json"
 
 # Run the dumpdata command inside the Docker container
-sudo docker exec -it app_backend_1 python manage.py dumpdata --exclude=auth.group --exclude=auth.permission --exclude=django_admin_log --output=/home/app/web/db_backup.json
+sudo docker exec -it app_backend_1 python manage.py dumpdata --exclude=auth.group --exclude=auth.permission --exclude=contenttypes --output=/home/app/web/db_backup.json
 sudo docker exec -it backend python manage.py dumpdata --output=$BACKUP_FILE
 
 sudo docker cp app_backend_1:/home/app/web/$FILE_NAME $BASE_PATH/$FILE_NAME
