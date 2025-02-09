ad47e1baffea7d2e20ca5d546681fc6d5000777f
Akasukpe Aghoghomena <aghoghomena3@gmail.com>
2025-02-09 09:28:24 -0500

    Added s3 to fetch initial database payload

--- scripts/fetch_env.sh
+++ scripts/fetch_env.sh
@@ -6,4 +6,6 @@ echo "Fetching environment variables from AWS SSM..."
 # Replace these with your Parameter Store paths
 aws ssm get-parameter --name "Portfolio" --with-decryption --query "Parameter.Value" --output text > .env
 
+aws s3 cp s3://menaportfolio/database/backup.json ../backup.json
+
 echo ".env file created successfully!"
